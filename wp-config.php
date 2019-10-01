f6b08a6ac92210cf7fbdad31242999d1eff577a2
Titus <titus22.j@gmail.com>
2019-10-01 09:12:26 +0200

    Added prefix to env

--- wp-config.php
+++ wp-config.php
@@ -63,7 +63,7 @@ define( 'NONCE_SALT',       getenv('DB_NONCE_SALT'));
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = 'wp_';
+$table_prefix = getenv('DB_PREFIX');
 
 /**
  * For developers: WordPress debugging mode.
